513371dd0b73971cabc7f38d89e1329ed44705e1
Sergiupetran <107993955+Sergiupetran@users.noreply.github.com>
2022-10-14 18:12:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function () {
+  document.querySelector(".submit-btn").addEventListener("click", function(){
+    document.querySelector(".second-container").style.display = "block";
+});
+  });
+  document.querySelector(".submit-btn").addEventListener("click", function(){
+    document.querySelector(".container").style.visibility = "hidden";
+  });
+  const btns = document.querySelectorAll(".number");
+  btns.forEach(btn => {
+    btn.addEventListener("click", function(){
+      this.classList.add("selected");
+      document.querySelector(".raiting-number").innerHTML = this.innerHTML;
+    });
+  });
